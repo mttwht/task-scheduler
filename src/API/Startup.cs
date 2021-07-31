aa01d1149d8d3dd84a4916fea9fcd791e3e520cf
Matt <Matthew.White02@gmail.com>
2021-07-31 11:34:03 +0100

    Run migrations on API startup

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MttWht.Scheduler.Persistence;
 
 namespace API
 {
@@ -31,6 +33,10 @@ namespace API
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+
+            services.AddDbContext<DataContext>(options => {
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
         }
 
